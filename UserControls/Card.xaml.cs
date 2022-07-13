32ab6bdf997a6645b29839388f261c2800990a8f
zjyWuDiPL <591102518@qq.com>
2022-07-13 23:27:36 +0800

    card 自定义空间添加

--- UserControls/Card.xaml.cs
+++ UserControls/Card.xaml.cs
@@ -24,5 +24,40 @@ namespace WpfApp1.UserControls
         {
             InitializeComponent();
         }
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+        public static readonly DependencyProperty TitleProperty = DependencyProperty.Register("Title", typeof(string), typeof(Card));
+
+        public bool IsChecked
+        {
+            get { return (bool)GetValue(IsCheckedProperty); }
+            set { SetValue(IsCheckedProperty, value); }
+        }
+        public static readonly DependencyProperty IsCheckedProperty = DependencyProperty.Register("IsChecked", typeof(string), typeof(Card));
+
+        public bool IsHorizontal
+        {
+            get { return (bool)GetValue(IsHorizontalProperty); }
+            set { SetValue(IsHorizontalProperty, value); }
+        }
+        public static readonly DependencyProperty IsHorizontalProperty = DependencyProperty.Register("IsHorizontal", typeof(string), typeof(Card));
+        public ImageSource ImageOn
+        {
+            get { return (ImageSource)GetValue(ImageOnProperty); }
+            set { SetValue(ImageOnProperty, value); }
+        }
+        public static readonly DependencyProperty ImageOnProperty = DependencyProperty.Register("ImageOn", typeof(string), typeof(Card));
+        public ImageSource ImageOff
+        {
+            get { return (ImageSource)GetValue(ImageOffProperty); }
+            set { SetValue(ImageOffProperty, value); }
+        }
+        public static readonly DependencyProperty ImageOffProperty = DependencyProperty.Register("ImageOff", typeof(string), typeof(Card));
+
+
     }
 }
